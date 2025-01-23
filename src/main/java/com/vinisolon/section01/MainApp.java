cd83d89edb94711573932f40a3c528734ebc934b
Vinicius Solon <vini.97@live.com>
2025-01-23 14:09:30 -0300

    Start!

---
+++ src/main/java/com/vinisolon/section01/MainApp.java
@@ -0,0 +1,13 @@
+package com.vinisolon.section01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisolon/section01/MainAppTests.java
@@ -0,0 +1,13 @@
+package com.vinisolon.section01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
